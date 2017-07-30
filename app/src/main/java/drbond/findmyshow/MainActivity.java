fcc42c3e59453d3d0c27164a402052aa3f754570
Ryan Bond <drbond@gmail.com>
2017-07-30 12:19:03 -0500

    initial project creation

---
+++ app/src/main/java/drbond/findmyshow/MainActivity.java
@@ -0,0 +1,17 @@
+package drbond.findmyshow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Hide the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
