650a2ab73440a882efbb41b716621eb7c1bbfb7e
Ryan Bond <drbond@gmail.com>
2017-07-30 19:41:38 -0500

    modifications to the gradle build, and testing some window view components

--- app/src/main/java/drbond/findmyshow/MainActivity.java
+++ app/src/main/java/drbond/findmyshow/MainActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         // Hide the title bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
